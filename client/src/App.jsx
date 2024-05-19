5c464306c91dd686021585b1939087833858a4d7
Rabia Kahya <rabiakahya22@gmail.com>
2024-05-19 08:11:10 +0300

    create react app and all installations

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500 text-xl'>App</div>
+  )
+}
+
+export default App

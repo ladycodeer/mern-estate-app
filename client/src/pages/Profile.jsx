49dbb0fab846d9dd679487d4f3c064595d61280c
Rabia Kahya <rabiakahya22@gmail.com>
2024-05-21 10:26:26 +0300

    sign out functionality added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -107,6 +109,21 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+  const handleSignOut = async () => {
+
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
   return (
 
     <div className='p-3 max-w-lg mx-auto'>
@@ -168,7 +185,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-slate-500 cursor-pointer'>Hesabı Sil</span>
-        <span className='text-slate-500 cursor-pointer'>Çıkış Yap</span>
+        <span  onClick={handleSignOut} className='text-slate-500 cursor-pointer'>Çıkış Yap</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>
